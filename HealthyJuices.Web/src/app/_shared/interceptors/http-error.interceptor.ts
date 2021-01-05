13e7a67156f37ab50e91e956d4f5b28d50f546ce
patryk.bujalla <patryk.bujalla@portacapena.com>
2021-01-05 18:36:23 +0100

    fix companies form

---
+++ HealthyJuices.Web/src/app/_shared/interceptors/http-error.interceptor.ts
@@ -0,0 +1,49 @@
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+      .pipe(
+        retry(0),
+        catchError((error: HttpErrorResponse) => {
+          console.error(error);
+          let message = '';
+          switch (error.status) {
+            case 406:
+            case 409:
+            case 500:
+              message = error.error;
+              break;
+            case 404:
+              message = '404 Server not found';
+              break;
+            case 401:
+              message = 'Unauthorized operation';
+              this.authService.logout();
+              break;
+            case 0:
+              message = 'Server can\'t be reached';
+              break;
+            default:
+              message = 'Unknown error';
+              break;
+          }
+          return throwError(message);
+        })
+      );
+  }
+}
--- HealthyJuices.Web/src/app/app.module.ts
+++ HealthyJuices.Web/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './_shared/interceptors/jwt.interceptor';
+import { HttpErrorInterceptor } from './_shared/interceptors/http-error.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { JwtInterceptor } from './_shared/interceptors/jwt.interceptor';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     MessageService,
   ],
   bootstrap: [AppComponent]
