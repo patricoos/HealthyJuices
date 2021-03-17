0df39803e837be0ad53ecdfdfaf2acf397a544c9
patryk.bujalla <patryk.bujalla@portacapena.com>
2021-03-17 12:58:09 +0100

    clean timeprovider

--- HealthyJuices.Backend/Common/HealthyJuices.Common/Services/TimeProvider.cs
+++ HealthyJuices.Backend/Common/HealthyJuices.Common/Services/TimeProvider.cs
@@ -7,31 +7,5 @@ namespace HealthyJuices.Common.Services
     {
         public DateTime Now => DateTime.Now;
         public DateTime UtcNow => DateTime.UtcNow;
-
-        public TimeZoneInfo LocalTimeZone => TimeZoneInfo.Local;
-
-        public DateTime ToLocalDateTime(DateTime dateTime)
-        {
-            if (dateTime.Kind == DateTimeKind.Local)
-                return dateTime;
-
-            if (dateTime.Kind != DateTimeKind.Utc)
-            {
-                dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
-            }
-            return TimeZoneInfo.ConvertTime(dateTime, LocalTimeZone);
-        }
-
-        public DateTime ToUtcDateTime(DateTime dateTime)
-        {
-            if (dateTime.Kind == DateTimeKind.Utc)
-                return dateTime;
-
-            if (dateTime.Kind != DateTimeKind.Local)
-            {
-                dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Local);
-            }
-            return TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Utc);
-        }
     }
 }
