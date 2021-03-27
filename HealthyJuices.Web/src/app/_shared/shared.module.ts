fc7569647befe982a295e2b172fb6e8aebd48fee
patryk.bujalla <patryk.bujalla@portacapena.com>
2021-03-27 11:02:56 +0100

    delete footer component

--- HealthyJuices.Web/src/app/_shared/shared.module.ts
+++ HealthyJuices.Web/src/app/_shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { HeaderComponent } from './components/main-layout/header/header.component';
-import { FooterComponent } from './components/main-layout/footer/footer.component';
 import { EmptyLayoutComponent } from './components/empty-layout/empty-layout.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { BreadcrumbComponent } from './components/main-layout/breadcrumb/breadcrumb.component';
@@ -15,7 +14,6 @@ import { SpaceBeforeCapitalPipe } from './pipes/space-before-capital.pipe';
   declarations: [
     MainLayoutComponent,
     HeaderComponent,
-    FooterComponent,
     EmptyLayoutComponent,
     LoaderComponent,
     BreadcrumbComponent,
